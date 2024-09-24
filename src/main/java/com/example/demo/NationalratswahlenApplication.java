fdb58a9dc67317131be6eaa481a2959b0afcdfdb
Swordpro3001 <puertofranz@gmail.com>
2024-09-24 14:04:19 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/NationalratswahlenApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationalratswahlenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NationalratswahlenApplication.class, args);
+	}
+
+}
